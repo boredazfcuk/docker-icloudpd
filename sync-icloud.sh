22283d5e4ff656c631f433e00987f7ff6830c668
boredazfcuk <boredazfcuk@hotmail.co.uk>
2019-09-06 00:17:58 +0100

    Cookie expiration date now extracted from cookie.
    Displays a warning if there are less than 7 days before the cookie expires. This is the variable checked in the healthcheck.
    Changed conditions from INFO to ERROR in cases where the script cannot continue.
    If script is run with the GENERATECOOKIE=True variable, script now removes cookie prior to generating the new one.

--- sync-icloud.sh
+++ sync-icloud.sh
@@ -6,11 +6,19 @@ COOKIE="${COOKIE//[^a-zA-Z0-9_]/}"
 COOKIE="$(echo -n "${COOKIE}" | tr A-Z a-z)"
 
 CheckCookie(){
-   if [ "$(find "${CONFIGDIR}" -type f -name "${COOKIE}" -mtime +55 | wc -l)" -ne 0 ]; then
-      find "${CONFIGDIR}" -type f -name "${COOKIE}" -mtime +55 -delete
+   if [ -f "${CONFIGDIR}/${COOKIE}" ]; then
+      if [ "$(grep -c "X-APPLE-WEBAUTH-HSA-TRUST" "${CONFIGDIR}/${COOKIE}")" -eq 1 ]; then
+         EXPIRE2FA="$(cat "${CONFIGDIR}/${COOKIE}" | grep "X-APPLE-WEBAUTH-HSA-TRUST" | sed -e 's#.*expires="\(.*\)"; HttpOnly.*#\1#')"
+         EXPIREWEB="$(cat "${CONFIGDIR}/${COOKIE}" | grep "X_APPLE_WEB_KB" | sed -e 's#.*expires="\(.*\)"; HttpOnly.*#\1#')"
+      else
+         while [ ! "$(grep -c "X-APPLE-WEBAUTH-HSA-TRUST" "${CONFIGDIR}/${COOKIE}")" -eq 1 ]; do
+            echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    Cookie has expired. Container must be run interactively to generate an authentication cookie - retry in 5 minutes"
+            sleep 300
+         done
+      fi
    fi
    while [ ! -f "${CONFIGDIR}/${COOKIE}" ]; do
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Cookie does not exist. Container must be run interactively to generate an authentication cookie - retry in 5 minutes"
+      echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    Cookie does not exist. Container must be run interactively to generate an authentication cookie - retry in 5 minutes"
       sleep 300
    done
 }
@@ -19,7 +27,7 @@ CreateGroup(){
    if [ -z "$(getent group ${GROUP} | cut -d: -f3)" ]; then
       addgroup -g "${GID}" "${GROUP}"
    elif [ ! "$(getent group "${GROUP}" | cut -d: -f3)" = "${GID}" ]; then
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Group GID mismatch - exiting"
+      echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    Group GID mismatch - exiting"
       exit 1
    fi
 }
@@ -28,31 +36,32 @@ CreateUser(){
    if [ -z "$(getent passwd "${USER}" | cut -d: -f3)" ]; then
       adduser -s /bin/ash -D -G "${GROUP}" -u "${UID}" "${USER}" -h "/home/$USER"
    elif [ ! "$(getent passwd "${USER}" | cut -d: -f3)" = "${UID}" ]; then
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     User UID mismatch - exiting"
+      echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    User UID mismatch - exiting"
       exit 1
    fi
 }
 
-if [ -z "${USER}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     User name invalid - exiting" && exit 1; fi
-if [ -z "${APPLEID}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Apple ID invalid - exiting" && exit 1; fi
-if [ -z "${APPLEPASSWORD}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Apple Password not specified - exiting" && exit 1; fi
-if [ -z "${GROUP}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Group name invalid - exiting" && exit 1; fi
-if [ -z "${GID}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Group ID invalid - exiting" && exit 1; fi
-if [ -z "${USER}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     User name invalid - exiting" && exit 1; fi
-if [ -z "${UID}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     User ID invalid - exiting" && exit 1; fi
+if [ -z "${USER}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    User name invalid - exiting" && exit 1; fi
+if [ -z "${APPLEID}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    Apple ID invalid - exiting" && exit 1; fi
+if [ -z "${APPLEPASSWORD}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    Apple Password not specified - exiting" && exit 1; fi
+if [ -z "${GROUP}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    Group name invalid - exiting" && exit 1; fi
+if [ -z "${GID}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    Group ID invalid - exiting" && exit 1; fi
+if [ -z "${USER}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    User name invalid - exiting" && exit 1; fi
+if [ -z "${UID}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    User ID invalid - exiting" && exit 1; fi
 
 if [ "${GENERATECOOKIE}" = "True" ]; then
    CreateGroup
    CreateUser
-   su "${USER}" -c "/usr/bin/icloudpd --username \"${APPLEID}\" --password=\"${APPLEPASSWORD}\" --cookie-directory=\"${CONFIGDIR}\""
+   if [ -f "${CONFIGDIR}/${COOKIE}" ]; then rm "${CONFIGDIR}/${COOKIE}"; fi
+   su "${USER}" -c "/usr/bin/icloudpd --username \"${APPLEID}\" --password \"${APPLEPASSWORD}\" --cookie-directory \"${CONFIGDIR}\""
    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Cookie generated. Sync should now be successful"
    exit 0
 fi
 
 CheckCookie
 
-if [ ! -f "/home/${USER}/iCloud/.mounted" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     iCloud folder not mounted - exiting" && exit 1; fi
-if [ -z "${INTERVAL}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Sync interval not specified - exiting" && exit 1; fi
+if [ ! -f "/home/${USER}/iCloud/.mounted" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    iCloud folder not mounted - exiting" && exit 1; fi
+if [ -z "${INTERVAL}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    Sync interval not specified - exiting" && exit 1; fi
 
 echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Local account: ${USER}-${UID}:${GROUP}-${GID}"
 echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Apple account: ${APPLEID}:${APPLEPASSWORD}"
@@ -72,10 +81,10 @@ fi
 
 while :; do
    CheckCookie
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Syncronisation for ${USER} started"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Syncronisation started for ${USER}"
    if [ -f "/home/${USER}/iCloud/.mounted" ]; then
       echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Sync ${USER} iCloud..."
-      su -s /bin/ash "${USER}" -c "/usr/bin/icloudpd --directory \"/home/${USER}/iCloud\" --cookie-directory=\"${CONFIGDIR}\" --username=\"${APPLEID}\" --password=\"${APPLEPASSWORD}\" \""${CLIOPTIONS}"\""
+      su -s /bin/ash "${USER}" -c "/usr/bin/icloudpd --directory /home/${USER}/iCloud --cookie-directory \"${CONFIGDIR}\" --username \"${APPLEID}\" --password \"${APPLEPASSWORD}\" \""${CLIOPTIONS}"\""
       if [ $? -ne 0 ]; then "$(date '+%Y-%m-%d %H:%M:%S') INFO     Exit code non-zero - Error: $?"; fi
       if [ "${SETDATETIMEEXIF}" = "True" ]; then
          echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Set correct file time stamp from exif data..."
@@ -94,11 +103,22 @@ while :; do
       if [ "$(find "/home/${USER}/iCloud" -type f ! -perm 660 | wc -l)" -ne 0 ]; then
          find "/home/${USER}/iCloud" -type f ! -perm 660 -exec chmod 660 {} +
       fi
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Syncronisation for ${USER} complete"
+      EXPIREWEB="${EXPIREWEB::-1}"
+      EXPIREWEB="${EXPIREWEB/ / @ }"
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Web page cookie expires: ${EXPIREWEB}"
+      EXPIRE2FA="${EXPIRE2FA::-1}"
+      EXPIRE2FASECS="$(date -d "${EXPIRE2FA}" '+%s')"
+      DAYSREMAINING="$(expr $((EXPIRE2FASECS - $(date '+%s'))) / 86400)"
+      EXPIRE2FA="${EXPIRE2FA/ / @ }"
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Two factor authentication cookie expires: ${EXPIRE2FA}"
+      if [ "${DAYSREMAINING}" -lt 7 ]; then
+         echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING  Only ${DAYSREMAINING} days until two factor authentication cookie expires - Please reinitialise"
+      fi
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Syncronisation complete for ${USER}"
       echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Next syncronisation at $(date +%H:%M -d "${INTERVAL} seconds")"
       sleep "${INTERVAL}"
    else
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Underlying volume is not mounted. Waiting a minute before retrying"
+      echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    Underlying volume is not mounted. Waiting a minute before retrying"
       sleep 60
    fi
 done
