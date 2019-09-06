562bb0812f35c5cbcaebb2db90ef2e03d4fa152d
boredazfcuk <boredazfcuk@hotmail.co.uk>
2019-09-06 19:38:09 +0100

    Prowl notification changes

--- sync-icloud.sh
+++ sync-icloud.sh
@@ -11,12 +11,12 @@ GetCookieName(){
 CheckCookie(){
    if [ -f "${CONFIGDIR}/${COOKIE}" ]; then
       if [ "$(grep -c "X-APPLE-WEBAUTH-HSA-TRUST" "${CONFIGDIR}/${COOKIE}")" -eq 1 ]; then
-         EXPIRE2FA="$(cat "${CONFIGDIR}/${COOKIE}" | grep "X-APPLE-WEBAUTH-HSA-TRUST" | sed -e 's#.*expires="\(.*\)"; HttpOnly.*#\1#')"
-         EXPIREWEB="$(cat "${CONFIGDIR}/${COOKIE}" | grep "X_APPLE_WEB_KB" | sed -e 's#.*expires="\(.*\)"; HttpOnly.*#\1#')"
+         EXPIRE2FA="$(grep "X-APPLE-WEBAUTH-HSA-TRUST" "${CONFIGDIR}/${COOKIE}" | sed -e 's#.*expires="\(.*\)"; HttpOnly.*#\1#')"
+         EXPIREWEB="$(grep "X_APPLE_WEB_KB" "${CONFIGDIR}/${COOKIE}" | sed -e 's#.*expires="\(.*\)"; HttpOnly.*#\1#')"
          EXPIREWEB="${EXPIREWEB::-1}"
          EXPIRE2FA="${EXPIRE2FA::-1}"
          EXPIRE2FASECS="$(date -d "${EXPIRE2FA}" '+%s')"
-         DAYSREMAINING="$(expr $((EXPIRE2FASECS - $(date '+%s'))) / 86400)"
+         DAYSREMAINING="$(($((EXPIRE2FASECS - $(date '+%s'))) / 86400))"
       else
          while [ ! "$(grep -c "X-APPLE-WEBAUTH-HSA-TRUST" "${CONFIGDIR}/${COOKIE}")" -eq 1 ]; do
             echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    Cookie has expired. Container must be run interactively to generate an authentication cookie - retry in 5 minutes"
@@ -66,10 +66,16 @@ CheckVariables(){
    if [ -z "${UID}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING  User ID not set, defaulting to '1000'"; UID="1000"; fi
    if [ -z "${GROUP}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING  Group name not set, defaulting to 'group'"; GROUP="group"; fi
    if [ -z "${GID}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING  Group ID not set, defaulting to '1000'"; GID="1000"; fi
-   if [ -z "${TZ}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING  Time zone not set, defaulting to Coordinated Universal Time 'UTC'"; TZ="UTC"; fi
+   if [ -z "${TZ}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING  Time zone not set, defaulting to Coordinated Universal Time 'UTC'"; export TZ="UTC"; fi
    if [ -z "${INTERVAL}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING  Syncronisation interval not set, defaulting to 1 day"; INTERVAL="86400"; fi
-   if [ -z "${APPLEID}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    Apple ID invalid - exiting" && exit 1; fi
-   if [ -z "${APPLEPASSWORD}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    Apple Password not specified - exiting" && exit 1; fi
+   if [ -z "${APPLEID}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    Apple ID not set - exiting"; exit 1; fi
+   if [ -z "${APPLEPASSWORD}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    Apple ID Password not set - exiting"; exit 1; fi
+   if [ "${NOTIFICATIONTYPE}" = "Prowl" ] &&  [ -z "${PROWLAPIKEY}" ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    Prowl notifications enabled, but Prowl API key not set - disabling notifications"
+      unset "${NOTIFICATIONTYPE}"
+   elif [ "${NOTIFICATIONTYPE}" = "Prowl" ] &&  [ ! -z "${PROWLAPIKEY}" ]; then
+         NEXTNOTIFICATION="$(date +%s)"
+   fi
 }
 
 CheckMount(){
@@ -110,21 +116,16 @@ SetDateTimeFromExif(){
    exiftool -d "%Y:%m:%d %H:%M:%S" '-FileModifyDate<createdate' '-FileModifyDate<creationdate' '-filemodifydate<datetimeoriginal' -if '(($datetimeoriginal and ($datetimeoriginal ne $filemodifydate)) or ($creationdate and ($creationdate ne $filemodifydate)))' -r -q "/home/${USER}/iCloud"
 }
 
-Notify(){
-   if [ "${NOTIFICATIONTYPE}" = "PROWL" ]; then
-      if [ ! -z "${PROWLAPIKEY}" ]; then
-         curl https://api.prowlapp.com/publicapi/add \
-            -F apikey="${PROWLAPIKEY}" \
-            -F application="$(date)" \
-            -F event="iCloud Photo Downloader" \
-            -F priority="${1}" \
-            -F description="${2}" \
-            >/dev/null 2>&1 &
-         echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Prowl notification sent - ${2}"
-      else
-         echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    API key not set, notification failed"
-      fi
-   fi
+NotifyProwl(){
+   curl https://api.prowlapp.com/publicapi/add \
+      -F apikey="${PROWLAPIKEY}" \
+      -F application="$(date)" \
+      -F event="iCloud Photo Downloader" \
+      -F priority="${1}" \
+      -F description="${2}" \
+      >/dev/null 2>&1 &
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Prowl notification sent - ${2}"
+   NEXTNOTIFICATION=$(date +%s -d "+24 hour")
 }
 
 ##### Script #####
@@ -141,6 +142,7 @@ SetOwnerAndGroup
 while :; do
    CheckCookie
    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Syncronisation started for ${USER}"
+   SYNCTIME="$(date +%s -d '+15 minutes')"
    if [ -f "/home/${USER}/iCloud/.mounted" ]; then
       echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Sync ${USER} iCloud..."
       su -s /bin/ash "${USER}" -c "/usr/bin/icloudpd --directory /home/${USER}/iCloud --cookie-directory \"${CONFIGDIR}\" --username \"${APPLEID}\" --password \"${APPLEPASSWORD}\" \""${CLIOPTIONS}"\""
@@ -154,8 +156,19 @@ while :; do
       echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Web page cookie expires: ${EXPIREWEB/ / @ }"
       echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Two factor authentication cookie expires: ${EXPIRE2FA/ / @ }"
       if [ "${DAYSREMAINING}" -lt 7 ]; then
-         echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING  Only ${DAYSREMAINING} days until two factor authentication cookie expires - Please reinitialise"
-         Notify "1" "Only ${DAYSREMAINING} days until two factor authentication cookie expires - Please reinitialise"
+         if [ "${SYNCTIME}" -gt "${NEXTNOTIFICATION}" ]; then
+            if [ "${DAYSREMAINING}" -eq 1 ]; then
+               echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING  Final day before two factor authentication cookie expires - Please reinitialise now"
+               if [ "${NOTIFICATIONTYPE}" = "Prowl" ]; then
+                  NotifyProwl "2" "Final day before two factor authentication cookie expires - Please reinitialise now"
+               fi
+            else
+               echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING  Only ${DAYSREMAINING} days until two factor authentication cookie expires - Please reinitialise"
+               if [ "${NOTIFICATIONTYPE}" = "Prowl" ]; then
+                  NotifyProwl "1" "Only ${DAYSREMAINING} days until two factor authentication cookie expires - Please reinitialise"
+               fi
+            fi
+         fi
       fi
       echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Syncronisation complete for ${USER}"
       echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Next syncronisation at $(date +%H:%M -d "${INTERVAL} seconds")"
