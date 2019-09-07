1a0406de8df6a95bcc73f5d7be442e0b5ab276c1
boredazfcuk <boredazfcuk@hotmail.co.uk>
2019-09-07 17:05:25 +0100

    Large Update - See Description
    Everything now separated into functions.
    Multiple functions combined.
    Improved cookie generation.
    Improved cookie inspection function.
    Better handling of 2FA/Web authentication methods.
    Bug-fix for multiple command line options.
    Removed find tests for permissions setting function to improve speed.

--- sync-icloud.sh
+++ sync-icloud.sh
@@ -16,33 +16,30 @@ CookieName(){
    COOKIE="$(echo -n "${COOKIE}" | tr A-Z a-z)"
 }
 
-CheckCookie(){
-   if [ $(grep -c -e "X-APPLE-WEBAUTH-HSA-TRUST" "${CONFIGDIR}/${COOKIE}") -eq 1 ]; then
-      AUTHTYPE="2FA"
-      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Authentication type not set, but two factor authentication cookie exists. Changing to 2FA"
-   else
-      AUTHTYPE="Web"
+CheckVariables(){
+   if [ -z "${USER}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING  User name not set, defaulting to 'user'"; USER="user"; fi
+   if [ -z "${UID}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING  User ID not set, defaulting to '1000'"; UID="1000"; fi
+   if [ -z "${GROUP}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING  Group name not set, defaulting to 'group'"; GROUP="group"; fi
+   if [ -z "${GID}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING  Group ID not set, defaulting to '1000'"; GID="1000"; fi
+   if [ -z "${INTERVAL}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING  Syncronisation interval not set, defaulting to 86400 seconds (1 day) "; INTERVAL="86400"; fi
+   if [ -z "${NOTIFICATIONDAYS}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING  Notification period not set, defaulting to 7 days"; NOTIFICATIONPERIOD="7"; fi
+   if [ -z "${TZ}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING  Time zone not set, defaulting to Coordinated Universal Time 'UTC'"; export TZ="UTC"; fi
+   if [ -z "${AUTHTYPE}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING  Authentication type not set, defaulting to two factor authentication"; AUTHTYPE="2FA"; fi
+   if [ "${NOTIFICATIONTYPE}" = "Prowl" ] && [ -z "${PROWLAPIKEY}" ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING  Prowl notifications enabled, but Prowl API key not set - disabling notifications"
+      unset "${NOTIFICATIONTYPE}"
+   elif [ "${NOTIFICATIONTYPE}" = "Prowl" ] && [ ! -z "${PROWLAPIKEY}" ]; then
+         NEXTNOTIFICATION="$(date +%s)"
    fi
-}
-
-Check2FAExpiration(){
-   EXPIRE2FA="$(grep "X-APPLE-WEBAUTH-HSA-TRUST" "${CONFIGDIR}/${COOKIE}" | sed -e 's#.*expires="\(.*\)"; HttpOnly.*#\1#')"
-   EXPIRE2FA="${EXPIRE2FA::-1}"
-   EXPIRE2FASECS="$(date -d "${EXPIRE2FA}" '+%s')"
-   DAYSREMAINING="$(($((EXPIRE2FASECS - $(date '+%s'))) / 86400))"
-   while [ "${DAYSREMAINING}" -lt 1 ]; do
-      echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    Cookie has expired. Container must be run interactively to generate an authentication cookie - retry in 5 minutes"
-      sleep 300
-   done
-}
-
-Generate2FACookie(){
-   if [ -f "${CONFIGDIR}/${COOKIE}" ]; then
-      rm "${CONFIGDIR}/${COOKIE}"
-   fi
-   su "${USER}" -c "/usr/bin/icloudpd --username \"${APPLEID}\" --password \"${APPLEPASSWORD}\" --cookie-directory \"${CONFIGDIR}\""
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Two factor authentication cookie generated. Sync should now be successful"
-   exit 0
+   if [ -z "${APPLEPASSWORD}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    Apple ID Password not set - exiting"; exit 1; fi
+   if [ -z "${APPLEID}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    Apple ID not set - exiting"; exit 1; fi
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Local user: ${USER}:${UID}"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Local group: ${GROUP}:${GID}"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Apple ID: ${APPLEID}"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Apple ID Password: ${APPLEPASSWORD}"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Cookie directory: ${CONFIGDIR}"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     iCloud directory: /home/${USER}/iCloud"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Command line options: ${CLIOPTIONS}"
 }
 
 CreateGroup(){
@@ -65,22 +62,13 @@ CreateUser(){
    fi
 }
 
-CheckVariables(){
-   if [ -z "${USER}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING  User name not set, defaulting to 'user'"; USER="user"; fi
-   if [ -z "${UID}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING  User ID not set, defaulting to '1000'"; UID="1000"; fi
-   if [ -z "${GROUP}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING  Group name not set, defaulting to 'group'"; GROUP="group"; fi
-   if [ -z "${GID}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING  Group ID not set, defaulting to '1000'"; GID="1000"; fi
-   if [ -z "${INTERVAL}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING  Syncronisation interval not set, defaulting to 1 day"; INTERVAL="86400"; fi
-   if [ -z "${TZ}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING  Time zone not set, defaulting to Coordinated Universal Time 'UTC'"; export TZ="UTC"; fi
-   if [ -z "${AUTHTYPE}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING  Authentication type not set, defaulting to two factor authentication"; AUTHTYPE="2FA"; fi
-   if [ "${NOTIFICATIONTYPE}" = "Prowl" ] && [ -z "${PROWLAPIKEY}" ]; then
-      echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING  Prowl notifications enabled, but Prowl API key not set - disabling notifications"
-      unset "${NOTIFICATIONTYPE}"
-   elif [ "${NOTIFICATIONTYPE}" = "Prowl" ] && [ ! -z "${PROWLAPIKEY}" ]; then
-         NEXTNOTIFICATION="$(date +%s)"
+Generate2FACookie(){
+   if [ -f "${CONFIGDIR}/${COOKIE}" ]; then
+      rm "${CONFIGDIR}/${COOKIE}"
    fi
-   if [ -z "${APPLEPASSWORD}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    Apple ID Password not set - exiting"; exit 1; fi
-   if [ -z "${APPLEID}" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    Apple ID not set - exiting"; exit 1; fi
+   su "${USER}" -c "/usr/bin/icloudpd --username \"${APPLEID}\" --password \"${APPLEPASSWORD}\" --cookie-directory \"${CONFIGDIR}\""
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Two factor authentication cookie generated. Sync should now be successful"
+   exit 0
 }
 
 CheckMount(){
@@ -90,29 +78,30 @@ CheckMount(){
    done
 }
 
-DisplayVariables(){
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Local user: ${USER}:${UID}"
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Local group: ${GROUP}:${GID}"
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Apple ID: ${APPLEID}"
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Apple ID Password: ${APPLEPASSWORD}"
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Cookie directory: ${CONFIGDIR}"
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     iCloud directory: /home/${USER}/iCloud"
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Command line options: ${CLIOPTIONS}"
-}
-
 SetOwnerAndGroup(){
    echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Set group and permissions of downloaded files..."
-   if [ "$(find "/home/${USER}/iCloud" ! -user "${USER}" | wc -l)" -ne 0 ]; then
-      find "/home/${USER}/iCloud" ! -user "${USER}" -exec chown "${USER}" {} \;
-   fi
-   if [ "$(find "/home/${USER}/iCloud" ! -group "${GROUP}" | wc -l )" -ne 0 ]; then 
-      find "/home/${USER}/iCloud" ! -group "${GROUP}" -exec chgrp "${GROUP}" {} \;
-   fi
-   if [ "$(find "/home/${USER}/iCloud" -type d ! -perm 770 | wc -l )" -ne 0 ]; then
-      find "/home/${USER}/iCloud" -type d ! -perm 770 -exec chmod 770 {} +
-   fi
-   if [ "$(find "/home/${USER}/iCloud" -type f ! -perm 660 | wc -l)" -ne 0 ]; then
-      find "/home/${USER}/iCloud" -type f ! -perm 660 -exec chmod 660 {} +
+   find "/home/${USER}/iCloud" ! -user "${USER}" -exec chown "${USER}" {} \;
+   find "/home/${USER}/iCloud" ! -group "${GROUP}" -exec chgrp "${GROUP}" {} \;
+   find "/home/${USER}/iCloud" -type d ! -perm 770 -exec chmod 770 {} +
+   find "/home/${USER}/iCloud" -type f ! -perm 660 -exec chmod 660 {} +
+}
+
+Check2FACookie(){
+   if [ -f "${CONFIGDIR}/${COOKIE}" ]; then
+      if [ $(grep -c "X-APPLE-WEBAUTH-HSA-TRUST" "${CONFIGDIR}/${COOKIE}") -eq 1 ]; then
+         EXPIRE2FA="$(grep "X-APPLE-WEBAUTH-HSA-TRUST" "${CONFIGDIR}/${COOKIE}" | sed -e 's#.*expires="\(.*\)"; HttpOnly.*#\1#')"
+         EXPIRE2FA="${EXPIRE2FA::-1}"
+         EXPIRE2FASECS="$(date -d "${EXPIRE2FA}" '+%s')"
+         DAYSREMAINING="$(($((EXPIRE2FASECS - $(date '+%s'))) / 86400))"
+         if [ "${DAYSREMAINING}" -gt 0 ]; then COOKIE2FAVALID="True"; fi
+         echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Valid two factor authentication cookie found"
+      else
+         echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    Cookie is not 2FA capable, authentication type may have changed. Please run container interactively to generate - Retry in 5 minutes"
+         sleep 300
+      fi
+   else
+      echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    Cookie does not exist. Please run container interactively to generate - Retry in 5 minutes"
+      sleep 300
    fi
 }
 
@@ -121,56 +110,9 @@ SetDateTimeFromExif(){
    exiftool -d "%Y:%m:%d %H:%M:%S" '-FileModifyDate<createdate' '-FileModifyDate<creationdate' '-filemodifydate<datetimeoriginal' -if '(($datetimeoriginal and ($datetimeoriginal ne $filemodifydate)) or ($creationdate and ($creationdate ne $filemodifydate)))' -r -q "/home/${USER}/iCloud"
 }
 
-NotifyProwl(){
-   curl https://api.prowlapp.com/publicapi/add \
-      -F apikey="${PROWLAPIKEY}" \
-      -F application="$(date)" \
-      -F event="iCloud Photo Downloader" \
-      -F priority="${1}" \
-      -F description="${2}" \
-      >/dev/null 2>&1 &
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Prowl notification sent - ${2}"
-   NEXTNOTIFICATION=$(date +%s -d "+24 hour")
-}
-
-##### Script #####
-CheckTerminal
-CookieName
-CheckVariables
-CreateGroup
-CreateUser
-if [ -z "${AUTHTYPE}" ] && [ -f "${CONFIGDIR}/${COOKIE}" ]; then CheckCookie; fi
-DisplayVariables
-if [ "${AUTHTYPE}" = "2FA" ] && [ ! -f "${CONFIGDIR}/${COOKIE}" ] && [ "${INTERACTIVE}" = "False" ]; then
-   echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    Two factor authentication enabled, but cookie does not exist"
-   while [ ! -f "${CONFIGDIR}/${COOKIE}" ]; do
-      echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    Please generate cookie using an interactive session - retry in 5 minutes"
-      sleep 300
-   done
-elif [ "${AUTHTYPE}" = "2FA" ] && [ ! -f "${CONFIGDIR}/${COOKIE}" ] && [ "${INTERACTIVE}" = "True" ]; then
-   Generate2FACookie
-fi
-if [ "${AUTHTYPE}" = "2FA" ]; then Check2FAExpiration; fi
-CheckMount
-SetOwnerAndGroup
-
-while :; do
-   if [ "${AUTHTYPE}" = "2FA" ]; then Check2FAExpiration; fi
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Syncronisation started for ${USER}"
-   SYNCTIME="$(date +%s -d '+15 minutes')"
-   CheckMount
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Sync ${USER} iCloud..."
-   su -s /bin/ash "${USER}" -c "/usr/bin/icloudpd --directory /home/${USER}/iCloud --cookie-directory \"${CONFIGDIR}\" --username \"${APPLEID}\" --password \"${APPLEPASSWORD}\" \""${CLIOPTIONS}"\""
-   if [ $? -ne 0 ]; then
-      echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    Exit code non-zero - Error: $?"
-   fi
-   if [ "${SETDATETIMEEXIF}" = "True" ]; then
-      SetDateTimeFromExif
-   fi
-   SetOwnerAndGroup
-   if [ "${AUTHTYPE}" = "2FA" ]; then 
+Display2FAExpiry(){
       echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Two factor authentication cookie expires: ${EXPIRE2FA/ / @ }"
-      if [ "${DAYSREMAINING}" -lt 7 ]; then
+      if [ "${DAYSREMAINING}" -lt "${NOTIFICATIONPERIOD}" ]; then
          if [ "${SYNCTIME}" -gt "${NEXTNOTIFICATION}" ]; then
             if [ "${DAYSREMAINING}" -eq 1 ]; then
                echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING  Final day before two factor authentication cookie expires - Please reinitialise now"
@@ -185,8 +127,50 @@ while :; do
             fi
          fi
       fi
-   fi
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Syncronisation complete for ${USER}"
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Next syncronisation at $(date +%H:%M -d "${INTERVAL} seconds")"
-   sleep "${INTERVAL}"
-done
+}
+
+NotifyProwl(){
+   curl https://api.prowlapp.com/publicapi/add \
+      -F apikey="${PROWLAPIKEY}" \
+      -F application="$(date)" \
+      -F event="iCloud Photo Downloader" \
+      -F priority="${1}" \
+      -F description="${2}" \
+      >/dev/null 2>&1 &
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Prowl notification sent - ${2}"
+   NEXTNOTIFICATION=$(date +%s -d "+24 hour")
+}
+
+SyncUser(){
+   while :; do
+      if [ "${AUTHTYPE}" = "2FA" ]; then
+         COOKIE2FAVALID=False
+         while [ "${COOKIE2FAVALID}" = "False" ]; do Check2FACookie; done
+      fi
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Syncronisation started for ${USER}"
+      SYNCTIME="$(date +%s -d '+15 minutes')"
+      CheckMount
+      su -s /bin/ash "${USER}" -c "/usr/bin/icloudpd --directory /home/${USER}/iCloud --cookie-directory ${CONFIGDIR} --username ${APPLEID} --password \"${APPLEPASSWORD}\" ${CLIOPTIONS}"
+      if [ $? -ne 0 ]; then
+         echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR    Exit code non-zero - Error: $?"
+      fi
+      if [ "${SETDATETIMEEXIF}" = "True" ]; then SetDateTimeFromExif; fi
+      SetOwnerAndGroup
+      if [ "${AUTHTYPE}" = "2FA" ]; then Display2FAExpiry; fi
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Syncronisation complete for ${USER}"
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Next syncronisation at $(date +%H:%M -d "${INTERVAL} seconds")"
+      sleep "${INTERVAL}"
+   done
+}
+
+##### Script #####
+echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     ***** iCoud_photo_downloader container started *****"
+CheckTerminal
+CookieName
+CheckVariables
+CreateGroup
+CreateUser
+if [ "${INTERACTIVE}" = "True" ]; then Generate2FACookie; fi
+CheckMount
+SetOwnerAndGroup
+SyncUser
