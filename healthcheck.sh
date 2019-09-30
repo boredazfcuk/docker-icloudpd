bc10b1c3adfe04c97483fab9b387dd52305161aa
Maarten De Wispelaere <maarten@bitprocessor.be>
2019-09-30 21:05:23 +0200

    fix healthcheck script (cookie folder)

--- healthcheck.sh
+++ healthcheck.sh
@@ -2,10 +2,11 @@
 
 EXPIRED=0
 ICLOUDPD=$(cat /tmp/EXIT_CODE)
+COOKIEDIR=/cookie
 
 if [ -z "${AUTHTYPE}" ] || [ "${AUTHTYPE}" = "2FA" ]; then
    COOKIE="$(echo -n ${APPLEID//[^a-zA-Z0-9]/} | tr '[:upper:]' '[:lower:]')"
-   EXPIRE2FA="$(grep "X-APPLE-WEBAUTH-HSA-TRUST" "${CONFIGDIR}/${COOKIE}" | sed -e 's#.*expires="\(.*\)Z"; HttpOnly.*#\1#')"
+   EXPIRE2FA="$(grep "X-APPLE-WEBAUTH-HSA-TRUST" "${COOKIEDIR}/${COOKIE}" | sed -e 's#.*expires="\(.*\)Z"; HttpOnly.*#\1#')"
    EXPIRE2FASECS="$(date -d "${EXPIRE2FA}" '+%s')"
    DAYSREMAINING="$(($((EXPIRE2FASECS - $(date '+%s'))) / 86400))"
    if [ -z "${NOTIFICATIONDAYS}" ]; then NOTIFICATIONDAYS=7; fi
