23d82c966a939c5b8afc8a29320684dca3f68383
boredazfcuk <boredazfcuk@hotmail.co.uk>
2020-01-12 00:23:31 +0000

    Manual push of any source code changes

--- healthcheck.sh
+++ healthcheck.sh
@@ -4,12 +4,12 @@ if [ "${ICLOUDPD}" != 0 ]; then
    echo "iCloud Photos Downloader Error: ${ICLOUDPD}"
    exit 1
 fi
+COOKIE="$(echo -n ${APPLEID//[^a-zA-Z0-9]/} | tr '[:upper:]' '[:lower:]')"
 if [ ! -f "${CONFIGDIR}/${COOKIE}" ]; then
 	echo "Error: Cookie does not exist. Please generate new cookie"
 	exit 1
 fi
 if [ ! -z "${AUTHTYPE}" ] && [ "${AUTHTYPE}" = "2FA" ]; then
-   COOKIE="$(echo -n ${APPLEID//[^a-zA-Z0-9]/} | tr '[:upper:]' '[:lower:]')"
    EXPIRE2FA="$(grep "X-APPLE-WEBAUTH-HSA-TRUST" "${CONFIGDIR}/${COOKIE}" | sed -e 's#.*expires="\(.*\)Z"; HttpOnly.*#\1#')"
    EXPIRE2FASECS="$(date -d "${EXPIRE2FA}" '+%s')"
    DAYSREMAINING="$(($((EXPIRE2FASECS - $(date '+%s'))) / 86400))"
@@ -19,7 +19,9 @@ if [ ! -z "${AUTHTYPE}" ] && [ "${AUTHTYPE}" = "2FA" ]; then
       exit 1
    fi
 elif [ ! -z "${AUTHTYPE}" ] && [ "${AUTHTYPE}" = "Web" ]; then
-   COOKIE="$(echo -n ${APPLEID//[^a-zA-Z0-9]/} | tr '[:upper:]' '[:lower:]')"
    EXPIREWEB="$(grep "X_APPLE_WEB_KB" "${CONFIGDIR}/${COOKIE}" | sed -e 's#.*expires="\(.*\)Z"; HttpOnly.*#\1#')"
    EXPIREWEBSECS="$(date -d "${EXPIREWEB}" '+%s')"
-   DAYSREMAINING="$(($((EXPIREWEBSECS - $(date '+%s'))) 
+   DAYSREMAINING="$(($((EXPIREWEBSECS - $(date '+%s'))) / 86400))"
+fi
+echo "iCloud Photos Downloader successful and ${AUTHTYPE} cookie valid for ${DAYSREMAINING} day(s)"
+exit 0
