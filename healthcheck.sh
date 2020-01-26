fe6129904ac3c465cb5b460bdf62301165ac2276
boredazfcuk <boredazfcuk@hotmail.co.uk>
2020-01-26 21:38:26 +0000

    Manual push of any source code changes

--- healthcheck.sh
+++ healthcheck.sh
@@ -10,18 +10,18 @@ if [ -f "/tmp/icloudpd/check_exit_code}" ] || [ -f "/tmp/icloudpd/download_exit_
    fi
    if [ -f "/tmp/icloudpd/check_exit_code}" ]; then
       check_exit_code="$(cat /tmp/icloudpd/check_exit_code)"
-      if [ "${check_exit_code}" -ne0 ]; then
+      if [ "${check_exit_code}" -ne 0 ]; then
          echo "File check error: ${check_exit_code}"
          exit 1
       fi
    fi
 fi
-cookie="$(echo -n "${apple_id//[^a-zA-Z0-9]/}" | tr '[:upper:]' '[:lower:]')"
+cookie="$(echo -n "${apple_id//[^a-zA-Z0-9]}" | tr '[:upper:]' '[:lower:]')"
 if [ ! -f "${config_dir}/${cookie}" ]; then
 	echo "Error: Cookie does not exist. Please generate new cookie"
 	exit 1
 fi
-if [ "${authentication_type}" ] && [ "${authentication_type}" = "2FA" ]; then
+if [ "${authentication_type:=2FA}" = "2FA" ]; then
    twofa_expire_date="$(grep "X-APPLE-WEBAUTH-HSA-TRUST" "${config_dir}/${cookie}" | sed -e 's#.*expires="\(.*\)Z"; HttpOnly.*#\1#')"
    twofa_expire_seconds="$(date -d "${twofa_expire_date}" '+%s')"
    days_remaining="$(($((twofa_expire_seconds - $(date '+%s'))) / 86400))"
@@ -30,10 +30,13 @@ if [ "${authentication_type}" ] && [ "${authentication_type}" = "2FA" ]; then
       echo "Error: Two-factor authentication cookie has expired"
       exit 1
    fi
-elif [ "${authentication_type}" ] && [ "${authentication_type}" = "Web" ]; then
+elif [ "${authentication_type}" = "Web" ]; then
    web_cookie_expire_date="$(grep "X_APPLE_WEB_KB" "${config_dir}/${cookie}" | sed -e 's#.*expires="\(.*\)Z"; HttpOnly.*#\1#')"
    web_cookie_expire_seconds="$(date -d "${web_cookie_expire_date}" '+%s')"
    days_remaining="$(($((web_cookie_expire_seconds - $(date '+%s'))) / 86400))"
+else
+   echo "Error: Authentication type not recognised"
+   exit 1
 fi
 echo "iCloud Photos Downloader successful and ${authentication_type} cookie valid for ${days_remaining} day(s)"
 exit 0
