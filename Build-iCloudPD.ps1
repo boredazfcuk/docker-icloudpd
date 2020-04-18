e4417c5ee8729d5250a6c826aac31d9c7e2f1008
boredazfcuk <boredazfcuk@hotmail.co.uk>
2020-04-18 23:16:14 +0100

    Please check change.log

--- Build-iCloudPD.ps1
+++
@@ -1,9 +0,0 @@
-docker pull alpine:latest
-
-# aarch=arm64v8
-docker build C:\Users\boredazfcuk\Documents\GitHub\docker-icloudpd\ -f C:\Users\boredazfcuk\Documents\GitHub\docker-icloudpd\Dockerfile.aarch -t boredazfcuk/icloudpd:arm64v8
-
-# armhf=arm32v7
-docker build C:\Users\boredazfcuk\Documents\GitHub\docker-icloudpd\ -f C:\Users\boredazfcuk\Documents\GitHub\docker-icloudpd\Dockerfile.armhf -t boredazfcuk/icloudpd:arm32v7
-
-docker push boredazfcuk/icloudpd
