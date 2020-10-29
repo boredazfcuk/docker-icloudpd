96867c214fb522a14ab0e14261b942f7e6cfca2c
boredazfcuk <boredazfcuk@hotmail.co.uk>
2020-10-29 19:48:28 +0000

    Please check change.log

--- sync-icloud.sh
+++ sync-icloud.sh
@@ -451,7 +451,7 @@ CorrectJPEGTimestamps(){
          echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Timestamp of JPEG file: ${jpeg_date}"
          if [ "${heic_date}" != "${jpeg_date}" ]; then
             echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Setting timestamp of ${heic_file%.HEIC}.JPG to ${heic_date}"
-            touch --reference="${heic_date}" "${heic_file%.HEIC}.JPG"       
+            touch --reference="${heic_file}" "${heic_file%.HEIC}.JPG"       
          fi
       fi
    done
