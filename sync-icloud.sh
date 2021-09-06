535d16f7f2c97b8580024789ad929f49c200f6e5
boredazfcuk <boredazfcuk@hotmail.co.uk>
2021-09-06 23:40:32 +0100

    Please check change.log

--- sync-icloud.sh
+++ sync-icloud.sh
@@ -528,7 +528,7 @@ ConvertAllHEICs(){
 
 ForceConvertAllHEICs(){
    echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING  Force convert all HEICs to JPEG. This could result in dataloss if JPG files have been edited on disk"
-   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Waiting for 2mins before progressing. Please stop the container now, if this is not what you want to do"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     Waiting for 2mins before progressing. Please stop the container now, if this is not what you want to do..."
    sleep 120
    for heic_file in $(find "${download_path}" -type f -name *.HEIC 2>/dev/null); do
       echo "$(date '+%Y-%m-%d %H:%M:%S') INFO     HEIC file found: ${heic_file}"
