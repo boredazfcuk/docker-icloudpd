213da64517fb3930cae744c2920c2e4c68dee6cb
Aemon Cao <bot960528@gmail.com>
2022-10-10 14:50:07 +0800

    fix bark notifications
    Removed escape characters in notification text

--- sync-icloud.sh
+++ sync-icloud.sh
@@ -978,9 +978,9 @@ Notify(){
          -F "message=${notification_message}")"   
    elif [ "${notification_type}" = "Bark" ]; then
       if [ "${notification_files_preview_count}" ]; then
-         bark_text="$(echo -e "${notification_icon} \n${notification_message}\nMost recent ${notification_files_preview_count} ${notification_files_preview_type} files:\n${notification_files_preview_text//_/\\_}")"
+         bark_text="$(echo -e "${notification_icon} ${notification_message} Most recent ${notification_files_preview_count} ${notification_files_preview_type} files: ${notification_files_preview_text//_/\\_}")"
       else
-         bark_text="$(echo -e "${notification_icon} \n${notification_message}")"
+         bark_text="$(echo -e "${notification_icon} ${notification_message}")"
       fi
       notification_result="$(curl --silent --output /dev/null --write-out "%{http_code}" "http://${bark_server}/push" \
          -H 'Content-Type: application/json; charset=utf-8' \
